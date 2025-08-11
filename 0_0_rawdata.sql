4663bfd859d263970ce035d53bfe4f25585fd038
andremanns <ammanns1@outlook.com>
2025-08-11 17:58:11 +0100

    Delete 0_0_rawdata.sql

--- 0_0_rawdata.sql
+++
@@ -1,7 +0,0 @@
-CREATE OR REPLACE TABLE
-  `sacred-atom-456611-t6.cyclistic_trips.tripdata_0_0_rawdata` AS
-SELECT
-  *
-FROM
-  `sacred-atom-456611-t6.cyclistic_trips.tripdata_*`
-WHERE _TABLE_SUFFIX BETWEEN '202405' AND '202504'
