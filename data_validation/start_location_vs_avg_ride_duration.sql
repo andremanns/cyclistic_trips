22052b02e34a6e4a4a52bb416e2bf6d4412ace15
Andre Manns <ammanns1@outlook.com>
2025-08-12 19:08:52 +0100

    start station vs avg duration bar chart for cyclistic report

---
+++ data_validation/start_location_vs_avg_ride_duration.sql
@@ -0,0 +1,26 @@
+-- Title: Start location vs avg ride duration
+-- Report Section: Data Validation
+
+--------------------------------------------------------------
+
+-- Start location vs avg ride duration: 1_5_1_outliers_lwr_removed
+SELECT
+  start_location,
+  ROUND(avg(trip_duration)/60) AS avg_trip_duration,
+  COUNT(*) AS no_of_trips
+FROM
+  `sacred-atom-456611-t6.cyclistic_trips.1_5_1_outliers_lwr_removed`
+GROUP BY
+  start_location
+
+------------------------------------------------------
+
+-- Start location vs avg ride duration: 3_1_1_analysis
+SELECT
+  start_location,
+  ROUND(avg(ride_duration_seconds)/60) AS avg_trip_duration,
+  COUNT(*) AS no_of_trips
+FROM
+  `sacred-atom-456611-t6.cyclistic_trips.3_1_1_analysis`
+GROUP BY
+  start_location
