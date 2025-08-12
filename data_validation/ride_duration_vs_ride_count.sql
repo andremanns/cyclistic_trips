5ecedb3f00e723b31448d7e47717a1fee7954e43
andremanns <ammanns1@outlook.com>
2025-08-12 20:56:59 +0100

    Update ride_duration_vs_ride_count.sql

--- data_validation/ride_duration_vs_ride_count.sql
+++ data_validation/ride_duration_vs_ride_count.sql
@@ -1,6 +1,8 @@
 -- Title: Ride Duration vs Ride Count
 -- Report Section: Data Validation
 
+=========================================================================
+
 -- Ride duration vs Ride Count: 1_5_1_outliers_lwr_removed
 SELECT
   COUNT(*) AS no_of_rides,
@@ -9,6 +11,8 @@ FROM
   `sacred-atom-456611-t6.cyclistic_trips.1_5_1_outliers_lwr_removed`
 GROUP BY 
 ROUND(timestamp_diff(ended_at, started_at, second)/60)
+
+--------------------------------------------------------------------------
 
 -- Ride duration vs Ride Count: 1_5_2_outliers_upr_removed
 SELECT
